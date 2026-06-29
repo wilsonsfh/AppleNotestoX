ac1895bb6b8cee8a50ef7d3cab9e8209e3f8b092
wilsonsfh <74759808+wilsonsfh@users.noreply.github.com>
2026-06-29 22:30:07 +0800

    build(video): embed Info.plist (NSSpeechRecognitionUsageDescription) via linker

--- Package.swift
+++ Package.swift
@@ -15,7 +15,17 @@ let package = Package(
         .executableTarget(
             name: "AppleNotestoX",
             dependencies: ["SwiftSoup", "KeychainAccess"],
-            resources: [.process("Resources")]
+            resources: [.process("Resources")],
+            linkerSettings: [
+                // Embed an Info.plist into the executable so the Speech framework can read
+                // NSSpeechRecognitionUsageDescription (it traps at runtime if missing).
+                .unsafeFlags([
+                    "-Xlinker", "-sectcreate",
+                    "-Xlinker", "__TEXT",
+                    "-Xlinker", "__info_plist",
+                    "-Xlinker", "AppleNotestoX-Info.plist"
+                ])
+            ]
         ),
         .testTarget(
             name: "AppleNotestoXTests",
